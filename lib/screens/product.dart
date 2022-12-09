6b340c545c993bb23e47d81807014806105efff5
codingwithhassan <codingwithme.hassan@gmail.com>
2022-12-09 17:40:08 +0500

    HotFix: product detail screen overflow issue

--- lib/screens/product.dart
+++ lib/screens/product.dart
@@ -24,12 +24,12 @@ class _ProductState extends State<Product> {
   @override
   void initState() {
     super.initState();
-      DummyJson.getProduct(id).then((product) {
-        setState(() {
-          productData = product;
-          isLoading = false;
-        });
+    DummyJson.getProduct(id).then((product) {
+      setState(() {
+        productData = product;
+        isLoading = false;
       });
+    });
   }
 
   @override
@@ -41,169 +41,170 @@ class _ProductState extends State<Product> {
         backgroundColor: Colors.lightGreen,
         title: const Text("Detail Screen"),
       ),
-      body: !isLoading ? Column(
-        children: [
-          Container(
-            height: screenHeight / 2.0,
-            decoration: BoxDecoration(
-              border: Border.all(),
-            ),
-            child: PageView.builder(
-              controller: controller,
-              itemCount: productData.images.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Image.network(productData.images[index]);
-              },
-            ),
-          ),
-          const SizedBox(
-            height: 15.0,
-          ),
-          SingleChildScrollView(
-            child: Container(
-                alignment: Alignment.topRight,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Text(
-                            productData.title.toString(),
-                            style: const TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green,
+      body: !isLoading
+          ? SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    height: screenHeight / 2.0,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: PageView.builder(
+                      controller: controller,
+                      itemCount: productData.images.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(productData.images[index]);
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Text(
+                              productData.title.toString(),
+                              style: const TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
                             ),
+                          ),
+                          // Container(
+                          //   alignment: Alignment.topRight,
+                          //   child: RatingBar.builder(
+                          //     initialRating: 3,
+                          //     minRating: 0,
+                          //     maxRating: 5,
+                          //     direction: Axis.horizontal,
+                          //     allowHalfRating: true,
+                          //     itemCount: 5,
+                          //     itemPadding:
+                          //         const EdgeInsets.symmetric(horizontal: 4.0),
+                          //     itemBuilder: (context, _) => const Icon(
+                          //       Icons.star,
+                          //       color: Colors.amber,
+                          //     ),
+                          //     onRatingUpdate: (rating) {
+                          //       print(rating);
+                          //     },
+                          //   ),
+                          // ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              r'Price: $' + productData.price.toString(),
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              r'Available Stock: ' +
+                                  productData.stock.toString(),
+                              style: const TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 20, left: 20, right: 20),
+                        child: Text(
+                          productData.description.toString(),
+                          style: const TextStyle(
+                            color: Colors.black54,
                           ),
                         ),
-                        // Container(
-                        //   alignment: Alignment.topRight,
-                        //   child: RatingBar.builder(
-                        //     initialRating: 3,
-                        //     minRating: 0,
-                        //     maxRating: 5,
-                        //     direction: Axis.horizontal,
-                        //     allowHalfRating: true,
-                        //     itemCount: 5,
-                        //     itemPadding:
-                        //         const EdgeInsets.symmetric(horizontal: 4.0),
-                        //     itemBuilder: (context, _) => const Icon(
-                        //       Icons.star,
-                        //       color: Colors.amber,
-                        //     ),
-                        //     onRatingUpdate: (rating) {
-                        //       print(rating);
-                        //     },
-                        //   ),
-                        // ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            r'Price: $' + productData.price.toString(),
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5.0,
-                          ),
-                          Text(
-                            r'Available Stock: ' + productData.stock.toString(),
-                            style: const TextStyle(
-                              color: Colors.black,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                const Text(
+                                  r'Categories:   ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Chip(
+                                  padding: const EdgeInsets.all(0),
+                                  backgroundColor: Colors.blue,
+                                  label: Text(productData.category.toString(),
+                                      style:
+                                          const TextStyle(color: Colors.white)),
+                                )
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 20, left: 20, right: 20),
-                      child: Text(
-                        productData.description.toString(),
-                        style: const TextStyle(
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            children: [
-                              const Text(
-                                r'Categories:   ',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
+                      Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                const Text(
+                                  r'Brands:   ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
                                 ),
-                              ),
-                              Chip(
-                                padding: const EdgeInsets.all(0),
-                                backgroundColor: Colors.blue,
-                                label: Text(productData.category.toString(),
-                                    style:
-                                        const TextStyle(color: Colors.white)),
-                              )
-                            ],
+                                Chip(
+                                  padding: const EdgeInsets.all(0),
+                                  backgroundColor: Colors.green,
+                                  label: Text(productData.brand.toString(),
+                                      style:
+                                          const TextStyle(color: Colors.white)),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            children: [
-                              const Text(
-                                r'Brands:   ',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              Chip(
-                                padding: const EdgeInsets.all(0),
-                                backgroundColor: Colors.green,
-                                label: Text(productData.brand.toString(),
-                                    style:
-                                        const TextStyle(color: Colors.white)),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                )),
-          ),
-        ],
-      ) : Container(
-        alignment: Alignment.center,
-        width: double.infinity,
-        height: double.infinity,
-        child: const CircularProgressIndicator(),
-      ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: double.infinity,
+              child: const CircularProgressIndicator(),
+            ),
     );
   }
 }
