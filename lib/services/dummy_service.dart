46b8e332e1022fc7cad19d14e5502f4fffd1bc5d
codingwithhassan <codingwithme.hassan@gmail.com>
2022-12-15 09:11:24 +0500

    resolved analyzer issues

--- lib/services/dummy_service.dart
+++ lib/services/dummy_service.dart
@@ -1,23 +1,25 @@
 import 'package:get/get.dart';
 
 class DummyService extends GetConnect implements GetxService {
-  late String token;
   final String serviceBaseUrl;
-  late Map<String, String> _mainHeaders;
 
-  DummyService({required this.serviceBaseUrl}){
-   baseUrl = serviceBaseUrl;
-   timeout = const Duration(seconds: 15);
-   _mainHeaders = {
-     'Content-Type': 'application/json; charset=UTF-8',
-   };
+  DummyService({required this.serviceBaseUrl});
+
+  @override
+  void onInit() {
+    httpClient.baseUrl = serviceBaseUrl;
+    httpClient.addRequestModifier<Object?>((request) {
+      request.headers['Content-Type'] = 'application/json; charset=UTF-8';
+      return request;
+    });
+    httpClient.timeout = const Duration(seconds: 15);
   }
 
   Future<Response> getData(String uri) async {
-    try{
+    try {
       Response response = await get(uri);
       return response;
-    }catch(e){
+    } catch (e) {
       return Response(statusCode: 500, statusText: e.toString());
     }
   }
